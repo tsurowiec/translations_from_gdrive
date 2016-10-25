e2b369f0cbeb8ac879f9338f7039da959cf4445d
Tomasz Surowiec <tsurowiec@gmail.com>
2016-10-25 12:47:27 +0200

    refactor to include empty leaf keys

--- src/ConvertCsv2Json.php
+++ src/ConvertCsv2Json.php
@@ -10,58 +10,44 @@ class ConvertCsv2Json
         fclose($file);
 
         $file = fopen('data.csv', 'r');
-        $ar = $en = [];
-        foreach(range(1,5) as $x) {
+        foreach (range(1, 5) as $x) {
             fgetcsv($file);
         }
-        $levels = 0;
-        $l1 = $l2 = $l3 = $l4 = $l5 = '';
+        $lines = $en = $ar = $n = [];
         while ($line = fgetcsv($file)) {
-            if($line[0]) {
-                $l1 = $line[0];
-                $levels = 1;
-            }
-            if($line[1]) {
-                $l2 = $line[1];
-                $levels = 2;
+            $lines[] = $line;
+        }
+        fclose($file);
 
-            }
-            if($line[2]) {
-                $l3 = $line[2];
-                $levels = 3;
-
-            }
-            if($line[3]) {
-                $l4 = $line[3];
-                $levels = 4;
-
-            }
-            if($line[4]) {
-                $l5 = $line[4];
-                $levels = 5;
-            }
-            if($line[5]) {
-                $val = $line[5];
-                switch ($levels) {
-                    case 1 : $en[$l1] = $val; break;
-                    case 2 : $en[$l1][$l2] = $val; break;
-                    case 3 : $en[$l1][$l2][$l3] = $val; break;
-                    case 4 : $en[$l1][$l2][$l3][$l4] = $val; break;
-                    case 5 : $en[$l1][$l2][$l3][$l4][$l5] = $val; break;
-                }
-            }
-            if($line[6]) {
-                $val = $line[6];
-                switch ($levels) {
-                    case 1 : $ar[$l1] = $val; break;
-                    case 2 : $ar[$l1][$l2] = $val; break;
-                    case 3 : $ar[$l1][$l2][$l3] = $val; break;
-                    case 4 : $ar[$l1][$l2][$l3][$l4] = $val; break;
-                    case 5 : $ar[$l1][$l2][$l3][$l4][$l5] = $val; break;
+        for ($i = 0; $i < count($lines); ++$i) {
+            $l = $lines[$i];
+            $level = self::level($l);
+            $n[$level] = $l[$level-1];
+            if ($i == count($lines) - 1 || self::isLeaf($lines[$i], $lines[$i + 1])) {
+                switch ($level) {
+                    case 1 :
+                        $en[$n[1]] = $l[5];
+                        $ar[$n[1]] = $l[6];
+                        break;
+                    case 2 :
+                        $en[$n[1]][$n[2]] = $l[5];
+                        $ar[$n[1]][$n[2]] = $l[6];
+                        break;
+                    case 3 :
+                        $en[$n[1]][$n[2]][$n[3]] = $l[5];
+                        $ar[$n[1]][$n[2]][$n[3]] = $l[6];
+                        break;
+                    case 4 :
+                        $en[$n[1]][$n[2]][$n[3]][$n[4]] = $l[5];
+                        $ar[$n[1]][$n[2]][$n[3]][$n[4]] = $l[6];
+                        break;
+                    case 5 :
+                        $en[$n[1]][$n[2]][$n[3]][$n[4]][$n[5]] = $l[5];
+                        $ar[$n[1]][$n[2]][$n[3]][$n[4]][$n[5]] = $l[6];
+                        break;
                 }
             }
         }
-        fclose($file);
 
         $file = fopen('/lang/en.json', 'w');
         fwrite($file, json_encode($en, JSON_PRETTY_PRINT));
@@ -71,6 +57,33 @@ class ConvertCsv2Json
         fwrite($file, json_encode($ar, JSON_PRETTY_PRINT));
         fclose($file);
 
-        echo('done...');
+        echo 'done...';
+    }
+
+    private static function isLeaf($current, $next)
+    {
+        return self::level($current) >= self::level($next);
+    }
+
+    private static function level($line)
+    {
+        $levels = 0;
+        if ($line[0]) {
+            $levels = 1;
+        }
+        if ($line[1]) {
+            $levels = 2;
+        }
+        if ($line[2]) {
+            $levels = 3;
+        }
+        if ($line[3]) {
+            $levels = 4;
+        }
+        if ($line[4]) {
+            $levels = 5;
+        }
+
+        return $levels;
     }
 }
